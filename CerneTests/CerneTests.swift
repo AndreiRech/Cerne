91e95c8c4500c3c55db3339da08d589361f91525
AndreiRech <andreirech22@outlook.com>
2025-09-03 16:20:55 -0300

    Initial Commit

---
+++ CerneTests/CerneTests.swift
@@ -0,0 +1,17 @@
+//
+//  CerneTests.swift
+//  CerneTests
+//
+//  Created by Andrei Rech on 03/09/25.
+//
+
+import Testing
+@testable import Cerne
+
+struct CerneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
