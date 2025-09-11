c55788f8d9959ec1be91b03a1a103c1fc1b25f35
K0W4 <kowaleskigabriel@gmail.com>
2025-09-11 17:44:19 -0300

    feat: inicializing the view

--- Cerne/Features/ContentView.swift
+++ Cerne/Features/ContentView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @State private var isShowingDetails: Bool = false
+        
+    let samplePin: Pin = {
+        let user = User(name: "Marina Carvalho", height: 1.65)
+        let tree = ScannedTree(species: "Ipê-amarelo", height: 15.0, dap: 0.8, totalCO2: 668)
+        return Pin(latitude: 0, longitude: 0, date: Date(), user: user, tree: tree)
+    }()
+
     var body: some View {
-        Text("Hi")
+        ZStack {
+            Image(.treeDefault)
+            
+            Button {
+                isShowingDetails.toggle()
+            } label: {
+                Text("Show details")
+            }
+        }
+        
+        .navigationTitle("Cerne")
+        .sheet(isPresented: $isShowingDetails) {
+            ScrollView{
+                PinDetailsView(pin: samplePin)
+                    .presentationDetents([.height(265), .height(475)])
+                    .presentationDragIndicator(.visible)
+            }
+            .scrollDisabled(true)
+        }
     }
+}
+
+#Preview {
+    ContentView()
 }
