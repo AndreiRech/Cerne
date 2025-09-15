8cf03ab1921ddd50378239325b2b38277910c457
Richard Fagundes Rodrigues <richardsiacc@Richards-MacBook-Pro.local>
2025-09-15 14:33:10 -0300

    test: Properly implemented and adjusted tests covering 100% of the ViewModel

---
+++ CerneTests/MapViewModelTests.swift
@@ -0,0 +1,100 @@
+//
+//  MapViewModelTests.swift
+//  CerneTests
+//
+//  Created by Richard Fagundes Rodrigues on 12/09/25.
+//
+
+import SwiftUI
+import Testing
+import MapKit
+@testable import Cerne
+
+struct MapViewModelTests {
+    
+    @Test func testOnMapAppearStartsServicesAndFetchesPins() async throws {
+        let locationService = MockLocationService()
+        let pinService = MockPinService()
+        let userService = MockUserService()
+        let treeService = MockScannedTreeService()
+        
+        pinService.pins = [
+            Pin(latitude: 1.0, longitude: 1.0, date: Date(), user: User(name: "User1", height: 1.70), tree: ScannedTree(species: "A", height: 10.0, dap: 1, totalCO2: 1)),
+            Pin(latitude: 10.0, longitude: 10.0, date: Date(), user: User(name: "User2", height: 1.64), tree: ScannedTree(species: "b", height: 8.32, dap: 2, totalCO2: 2))
+        ]
+        
+        let viewModel = MapViewModel(locationService: locationService,
+                               pinService: pinService,
+                               userService: userService,
+                               scannedTreeService: treeService)
+
+        viewModel.onMapAppear()
+
+        #expect(locationService.startCalled == true)
+        
+        #expect(viewModel.pins.isEmpty == false)
+    }
+    
+    @Test func testRefreshLocation() async throws {
+        let locationService = MockLocationService()
+        let pinService = MockPinService()
+        let userService = MockUserService()
+        let treeService = MockScannedTreeService()
+        let mockLocation = UserLocation(latitude: -23.5, longitude: -46.6)
+        
+        locationService.userLocation = mockLocation
+        
+        let viewModel = MapViewModel(locationService: locationService,
+                               pinService: pinService,
+                               userService: userService,
+                               scannedTreeService: treeService)
+        
+        viewModel.refreshLocation()
+        
+        #expect(viewModel.userLocation != nil)
+        #expect(viewModel.userLocation?.latitude == mockLocation.latitude)
+        #expect(viewModel.userLocation?.longitude == mockLocation.longitude)
+    }
+    
+    @Test func testGetPins() async throws {
+//        let locationService = MockLocationService(userLocation: UserLocation(latitude: -23.5, longitude: -46.6))
+        let locationService = MockLocationService()
+        let pinService = MockPinService()
+        let userService = MockUserService()
+        let treeService = MockScannedTreeService()
+        
+        let expectedPins = [
+            Pin(latitude: -23.5, longitude: -46.6, date: Date(), user: User(name: "User 1", height: 2.65), tree: ScannedTree(species: "asmucha lagusta", height: 10.0, dap: 50.0, totalCO2: 168.0)),
+            Pin(latitude: -23.5, longitude: -46.6, date: Date(), user: User(name: "User 2", height: 2.65), tree: ScannedTree(species: "uaiviaja naozé", height: 10.0, dap: 50.0, totalCO2: 168.0)),
+        ]
+        
+        pinService.pins = expectedPins
+
+        let viewModel = MapViewModel(locationService: locationService,
+                               pinService: pinService,
+                               userService: userService,
+                               scannedTreeService: treeService)
+        
+        viewModel.getPins()
+        
+        #expect(viewModel.pins.count > 0)
+        #expect(viewModel.pins.first?.tree.species == "asmucha lagusta")
+    }
+    
+    @Test func testGetPinsFailure() async throws {
+        let locationService = MockLocationService()
+        let pinService = MockPinService(shouldFail: true)
+        let userService = MockUserService()
+        let treeService = MockScannedTreeService()
+
+        let viewModel = MapViewModel(locationService: locationService,
+                               pinService: pinService,
+                               userService: userService,
+                               scannedTreeService: treeService)
+        
+        viewModel.getPins()
+        
+        #expect(viewModel.pins.isEmpty)
+    }
+    
+}
