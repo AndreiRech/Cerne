c46e179bc491b2fcd8196050006bbf8652a0b8ed
AndreiRech <andreirech22@outlook.com>
2025-09-19 13:50:03 -0300

    fix: tests that werent needed

--- CerneTests/MockServices/MockLocationService.swift
+++ CerneTests/MockServices/MockLocationService.swift
@@ -8,10 +8,17 @@
 import Testing
 import MapKit
 @testable import Cerne
+import Combine
 
 class MockLocationService: LocationServiceProtocol {
+    var userLocationPublisher: AnyPublisher<UserLocation, Never>
+    
     var userLocation: UserLocation?
     var startCalled = false
+    
+    init() {
+        userLocationPublisher = PassthroughSubject<UserLocation, Never>().eraseToAnyPublisher()
+    }
     
     func start() {
         startCalled = true
