0fc8816e9603f4cb9f286d1b514981d3793d0f50
AndreiRech <andreirech22@outlook.com>
2025-09-26 17:49:42 -0300

    fix: text fix

--- Cerne/Features/Onboarding/OnboardingViewModel.swift
+++ Cerne/Features/Onboarding/OnboardingViewModel.swift
@@ -20,8 +20,8 @@ class OnboardingViewModel: OnboardingViewModelProtocol {
     
     let onboardingPages: [OnboardingPage] = [
         OnboardingPage(image: .onboarding1, title: "Descubra quanto carbono cada árvore pode capturar", description: "Use realidade aumentada para identificar a espécies das árvores e calcular o capacidade de sequestro de CO₂"),
-        OnboardingPage(image: .onboarding2, title: "Acompanhe a floresta crescendo bem pertinho de você", description: "CSeus registros fortalecem um banco de dados vivo que revela a biodiversidade e o carbono capturado em todo o mundo"),
-        OnboardingPage(image: .onboarding3, title: "Faça parte da comunidade", description: "VSome seu impacto ao de milhares de pessoas que ajudam a neutralizar CO₂")
+        OnboardingPage(image: .onboarding2, title: "Acompanhe a floresta crescendo bem pertinho de você", description: "Seus registros fortalecem um banco de dados vivo que revela a biodiversidade e o carbono capturado em todo o mundo"),
+        OnboardingPage(image: .onboarding3, title: "Faça parte da comunidade", description: "Some seu impacto ao de milhares de pessoas que ajudam a neutralizar CO₂")
     ]
     
     init(userDefaultService: UserDefaultServiceProtocol, userService: UserServiceProtocol) {
