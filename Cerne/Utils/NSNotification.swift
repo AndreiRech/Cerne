4120249ad2f1b843a2722da27ed1a0b2ea371dd5
AndreiRech <andreirech22@outlook.com>
2025-09-30 17:33:53 -0300

    feat: new extension for notification

---
+++ Cerne/Utils/NSNotification.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotification.swift
+//  Cerne
+//
+//  Created by Andrei Rech on 30/09/25.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let didUpdateUserData = NSNotification.Name("didUpdateUserData")
+}
